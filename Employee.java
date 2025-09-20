0a503feb4a51e2401a4e7f4d6e0a44ee70947155
Naganjali11 <naganjali425@gmail.com>
2025-09-20 22:50:26 +0530

    javafullstackprograms

---
+++ Employee.java
@@ -0,0 +1,13 @@
+package com.codegnan.oop.overloading;
+
+public class Employee {
+	String name;
+	double salary;
+	public Employee() {
+	}
+	public void displayDetails() {
+		System.out.println("Name:" +name);
+		System.out.println("Salary:"+salary);
+	}
+
+}
