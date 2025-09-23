a5f2190e5700ba5d7cae24ac1a45b86b708c6f28
Naganjali11 <naganjali425@gmail.com>
2025-09-23 11:19:42 +0530

    javafullstackprograms

---
+++ Test.java
@@ -0,0 +1,13 @@
+package com.codegnan.oop.overloading;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Child c = new Child();
+		c.display();
+
+
+	}
+
+}
