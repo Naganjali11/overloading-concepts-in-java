db85eae6fd2d326324b8ea058986ff10c0e1fac5
Naganjali11 <naganjali425@gmail.com>
2025-09-23 11:20:09 +0530

    javafullstackprograms

---
+++ Employee01.java
@@ -0,0 +1,15 @@
+package com.codegnan.oop.overloading;
+
+public class Employee01 {
+	String employeeName;
+	String designation;
+	int yearsOfExperince;
+	
+	public Employee01(String employeeName, String designation, int yearsOfExperince) {
+		super();
+		this.employeeName = employeeName;
+		this.designation = designation;
+		this.yearsOfExperince = yearsOfExperince;
+	}
+
+}
