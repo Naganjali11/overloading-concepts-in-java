8e0f18c4432b1c8620bea82a67ca7948399f5b59
Naganjali11 <naganjali425@gmail.com>
2025-09-23 11:20:50 +0530

    javafullstackprograms

---
+++ EmployeeDetails.java
@@ -0,0 +1,14 @@
+package com.codegnan.oop.overloading;
+
+public class EmployeeDetails {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		EmployeeInfo info=new EmployeeInfo();
+		info.inputEmployeeDetails();
+		info.displayEmployeeInfo();
+		info.calculateSalary();
+
+	}
+
+}
