a6a1f5faecc0ca6afea497c8be0fd30d737af92b
Naganjali11 <naganjali425@gmail.com>
2025-09-23 11:18:31 +0530

    javafullstackprograms

---
+++ Parent.java
@@ -0,0 +1,13 @@
+package com.codegnan.oop.overloading;
+
+public class Parent {
+	int x=10;
+    Parent(){
+  	  System.out.println("Parent Constructor");
+	
+    }
+    public void display() {
+        System.out.println("Parent class display method");
+    }
+
+}
